858ceef951254974fa48b6f7149b29d63a79721f
tsingxu <3x3h3q@gmail.com>
2012-10-29 18:52:36 +0800

    Na
    Signed-off-by: tsingxu <3x3h3q@gmail.com>

--- src/src/com/tsingxu/pitaya/Hello.java
+++ src/src/com/tsingxu/pitaya/Hello.java
@@ -1,14 +1,25 @@
 package com.tsingxu.pitaya;
 
+/**
+ * <b>in_a_word_briefly</b>
+ * 
+ * <ol>
+ * <li>...</li>
+ * </ol>
+ * 
+ * @since Oct 29, 2012 6:50:59 PM
+ * @author xuhuiqing
+ */
 public class Hello
 {
-
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args)
 	{
-		System.out.println("Hello world");
+		System.out
+				.println("Hello pitaya\nintro:\ta framework that aims to provide a high-performance & high-scalability web server, it's a trial.\nauthor:\txuhuiqing(tsingxu)");
+
 	}
 
 }
