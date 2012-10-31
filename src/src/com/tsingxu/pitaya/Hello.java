356f9dabd49f701e4dad9a08901851205fe17509
tsingxu <3x3h3q@gmail.com>
2012-10-31 18:04:44 +0800

    Signed-off-by: tsingxu <3x3h3q@gmail.com>

--- src/src/com/tsingxu/pitaya/Hello.java
+++ src/src/com/tsingxu/pitaya/Hello.java
@@ -21,57 +21,13 @@ public class Hello
 	 */
 	public static void main(String[] args)
 	{
-		logger.info("hello world");
-		logger.error("helsdafsadfsdlo world");
-		logger.warn("hello fds");
-		logger.fatal("sdf world");
-		logger.debug("hello world");
-		logger.info("hellofsdsdsdf world");
-		logger.error("hello world");
-		logger.warn("hellsdfo world");
-		logger.fatal("hellsfdsfdsdfsdfafsdfo world");
-		logger.debug("hello world");
-		logger.info("helasdfasdfasdfasdflo world");
-		logger.error("hello dsdsfdsworld");
-		logger.warn("hello world");
-		logger.fatal("hellodsfds world");
-		logger.debug("hello world");
-		logger.info("hello wdsfsdfdorld");
-		logger.error("hello dfdsworld");
-		logger.warn("hello world");
-		logger.fatal("hello world");
-		logger.debug("helloasdfasd world");
-		logger.info("hello world");
-		logger.error("hello world");
-		logger.warn("hello fsdfsdworld");
-		logger.fatal("hellasdfasdfasdfasdfofasdf world");
-		logger.debug("hello world");
-		logger.info("hellosdfsdf world");
-		logger.error("helloasdf world");
-		logger.warn("hello world");
-		logger.fatal("hellsdfdsssdo world");
-		logger.debug("hello world");
-		logger.info("hello world");
-		logger.error("hello world");
-		logger.warn("hellosdasdfsddsfasdfasdf world");
-		logger.fatal("hello world");
-		logger.debug("hello world");
-		logger.info("hello world");
-		logger.error("hello world");
-		logger.warn("hello world");
-		logger.fatal("hello world");
-		logger.debug("hello world");
-		logger.info("hello sddwfsdfddddddddddddddddddorld");
-		logger.error("hello world");
-		logger.warn("hello world");
-		logger.fatal("ddddasdfasdddddf world");
-		logger.debug("hello world");
-		logger.info("hello world");
-		logger.error("hellofffasdfasdf world");
-		logger.warn("hello world");
-		logger.fatal("sf world");
-		logger.debug("hello world");
-		// logger.info("Hello pitaya\nintro:\ta framework that aims to provide a high-performance & high-scalability web server, it's a trial.\nauthor:\txuhuiqing(tsingxu)");
+		String ls = System.getProperty("line.separator");
+		logger.info(ls + "---------------------------------------------------------------------"
+				+ ls + "                          Hello pitaya" + ls
+				+ "    intro: a framework that aims to provide a high-performance" + ls
+				+ "           & high-scalability web server, it's a trial." + ls
+				+ "    author:xuhuiqing (tsingxu)" + ls
+				+ "---------------------------------------------------------------------");
 	}
 
 }
