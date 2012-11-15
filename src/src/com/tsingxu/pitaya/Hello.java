a566d4c6e2bbe1207947aed16251b00e8c47bf5c
tsingxu <3x3h3q@gmail.com>
2012-11-15 11:23:18 +0800

    Signed-off-by: tsingxu <3x3h3q@gmail.com>

--- src/src/com/tsingxu/pitaya/Hello.java
+++ src/src/com/tsingxu/pitaya/Hello.java
@@ -22,15 +22,15 @@ public class Hello
 	public static void main(String[] args)
 	{
 		String ls = System.getProperty("line.separator");
-		String message = ls
+		String message = ls + ls
 				+ "---------------------------------------------------------------------" + ls
 				+ "                          Hello pitaya" + ls
 				+ "    intro: a framework that aims to provide a high-performance" + ls
 				+ "           & high-scalability web server, it's a trial." + ls
 				+ "    author:xuhuiqing (tsingxu)" + ls
-				+ "---------------------------------------------------------------------";
+				+ "---------------------------------------------------------------------" + ls;
 		logger.info(message);
-		System.out.println(message.substring(2));
-		logger.error("紧急事件，请联系110");
+		System.out.println(message.substring(4));
+		logger.error("紧急事件，请联系110。记住请拨110!" + message);
 	}
 }
